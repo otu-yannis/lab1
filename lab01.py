da0fef57cf72e1d8027d7f27db2a8f5fabe1f449
otu-yannis <144347269+otu-yannis@users.noreply.github.com>
2023-09-14 10:22:54 -0400

    Initial commit

---
+++ lab01.py
@@ -0,0 +1,12 @@
+def main():
+    cost_per_item = 19.99
+    quantity = 5 
+
+    # YOUR CODE TO CALCULATE THE TOTALS GOES HERE    
+
+    print(f'cost_per_item = ${cost_per_item:0.2f}') # a sample for you to use for the other prices
+
+    # YOUR CODE TO PRINT THE TOTALS GOES HERE
+
+if __name__ == "__main__":
+    main()
